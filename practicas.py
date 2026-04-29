2ac4adfa396c4dce3441ea669f3f58a261776fbc
alejandroj0620-crypto <alejandroj0620@gmail.com>
2026-04-29 10:26:04 -0500

    Update practicas.py

--- practicas.py
+++ practicas.py
@@ -109,3 +109,78 @@ class Asesoria(Servicio):
     def calcular_costo(self, cantidad, tipo):
         horas = self.convertir_tiempo(cantidad, tipo)
         return self.precio_base * horas * 1.2
+# ============= RESERVAS ============
+class Reserva():
+    
+    reservas=[]
+    
+    def __init__(self, cliente, servicio, cantidad, tipo_tiempo):
+        
+        if cantidad <= 0:
+            raise Exception("Tiempo inválido")
+        
+        self._cliente = cliente
+        self._servicio = servicio
+        self._cantidad = cantidad
+        self._tipo_tiempo = tipo_tiempo
+        self._estado = "pendiente"
+        
+    # --------- ESTADO DE LA RESERVA ---------
+    def confirmar(self):
+        try:
+            if self._estado != "pendiente":
+                raise ReservaError ("Solo reservas pendientes pueden confirmarse")
+            
+            self._estado = "confirmada"
+        except ReservaError as e:
+            Logger.log(f"Error al confirmar: {e}")
+            raise
+        
+    def cancelar(self):
+        try:
+            if self._estado == "cancelada":
+                raise ReservaError("La reserva ya está cancelada")
+            
+            self._estado = "cancelada"
+        except ReservaError as e:
+            Logger.log(f"Error al cancelar: {e}")
+            raise
+
+    def procesar(self):
+        try:
+            if self._estado != "confirmada":
+                raise ReservaError("Debe confirmar la reserva antes de procesarla")
+            
+            costo = self.servicio.calcular_costo(self.cantidad, self.tipo_tiempo)
+            
+            self._estado = "procesada"
+            return costo
+        
+        except Exception as e:
+            Logger.log(f"Error al procesar: {e}")
+            raise ReservaError("error en procesamiento") from e
+    
+    # --------- GUARDAR Y MOSTRAR ---------
+    
+    def guardar(self):
+        try:
+            Reserva.reservas.append(self)
+        except Exception as e:
+            Logger.log(f"Error al guardar reserva: {e}")
+            raise ReservaError("Error al guardar reserva") from e
+    
+    def mostrar(self):
+        return f"{self._cliente.mostrar()} | {self._servicio.mostrar()} | Estado: {self._estado}"
+
+# ================== SISTEMA ==================
+class Sistema:
+    def __init__(self):
+        self.clientes = []
+        self.servicios = []
+        self.reservas = []
+
+    def agregar_cliente(self, cliente):
+        self.clientes.append(cliente)
+
+    def crear_reserva(self, reserva):
+        self.reservas.append(reserva)
